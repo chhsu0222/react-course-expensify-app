867ccb2fcf4a46b74f4eb6ea8370981b67280012
ch <ch@mail.startek-eng.com>
2019-07-16 17:43:41 +0800

    Reducers
    sec. 10, video 91
    
    https://redux.js.org/basics/reducers
    
    Reducers:
    	1. Reducers are pure functions. The output is only determined by the input.
           It doesn't use anything else from outside of the function scope and it doesn't
           change anything outside of the function scope either.
        2. Never change state or action.

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -21,7 +21,9 @@ const resetCount = () => ({
     type: 'RESET'
 })
 
-const store = createStore((state = { count: 0 }, action) => {
+// Reducers
+
+const countReducer = (state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return {
@@ -42,7 +44,9 @@ const store = createStore((state = { count: 0 }, action) => {
         default:
             return state
     }
-})
+}
+
+const store = createStore(countReducer)
 
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState())
