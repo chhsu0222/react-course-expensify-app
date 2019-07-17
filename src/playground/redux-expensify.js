c0619889182a535010ddda4f6459914cc18e8794
ch <ch@mail.startek-eng.com>
2019-07-17 16:33:41 +0800

    Spreading Objects
    sec. 10, video 94
    
    Note that the object spread operator has not made its way into main stream. So we need install the babel plugin to support it.
    
    https://babeljs.io/docs/en/babel-plugin-transform-object-rest-spread.html
    
    $ npm i babel-plugin-transform-object-rest-spread@6.23.0
    
    Then add the plugin into .babelrc
    Now we can use the spread operator to update (override) the expense & filters without modifying the original object.

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -26,6 +26,19 @@ const removeExpense = ({ id } = {}) => ({
     id
 })
 
+// EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+})
+
+// SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+})
+
 // Expenses Reducer
 
 const expensesReducerDefaultState = []
@@ -39,6 +52,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
             ]
         case 'REMOVE_EXPENSE':
             return state.filter(({ id }) => id !== action.id)
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    }
+                } else {
+                    return expense
+                }
+            })
         default:
             return state
     }
@@ -47,7 +71,7 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
 // Filter Reducer
 
 const filtersReducerDefaultState = {
-    test: '',
+    text: '',
     sortBy: 'date',
     startDate: undefined,
     endDate: undefined
@@ -55,6 +79,11 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            }
         default:
             return state
     }
@@ -77,6 +106,10 @@ const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 
 const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }))
 
 store.dispatch(removeExpense({ id: expenseOne.expense.id }))
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }))
+
+store.dispatch(setTextFilter('rent'))
+store.dispatch(setTextFilter())
 
 const demoState = {
     expenses: [{
