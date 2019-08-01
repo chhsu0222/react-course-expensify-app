cfee3087b5055b2bafc78f0e320f41c7f9b583c4
ch <ch@mail.startek-eng.com>
2019-08-01 15:44:38 +0800

    Testing ExpenseListFilters: part II
    sec. 12, video 127

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -34,3 +34,45 @@ test('should render ExpenseListFilters with alt data correctly', () => {
     })
     expect(wrapper).toMatchSnapshot()
 })
+
+test('should handle text change', () => {
+    const value = 'new text'
+    wrapper.find('input').simulate('change', {
+        target: { value }
+    })
+    expect(setTextFilter).toHaveBeenLastCalledWith(value)
+})
+
+test('should sort by date', () => {
+    const value = 'date'
+    wrapper.setProps({
+        filters: altfilters
+    })
+    wrapper.find('select').simulate('change', {
+        target: { value }
+    })
+    expect(sortByDate).toHaveBeenCalled()
+})
+
+test('should sort by amount', () => {
+    const value = 'amount'
+    wrapper.find('select').simulate('change', {
+        target: { value }
+    })
+    expect(sortByAmount).toHaveBeenCalled()
+})
+
+test('should handle date changes', () => {
+    wrapper.find('DateRangePicker').prop('onDatesChange')({
+        startDate: altfilters.startDate,
+        endDate: altfilters.endDate
+    })
+    expect(setStartDate).toHaveBeenLastCalledWith(altfilters.startDate)
+    expect(setEndDate).toHaveBeenLastCalledWith(altfilters.endDate)
+})
+
+test('should handle date focus changes', () => {
+    const calendarFocused = 'endDate'
+    wrapper.find('DateRangePicker').prop('onFocusChange')(calendarFocused)
+    expect(wrapper.state('calendarFocused')).toBe(calendarFocused)
+})
