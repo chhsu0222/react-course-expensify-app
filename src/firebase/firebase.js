8d837c51d6026d37d94ad7b4677e45c151a925f8
ch <ch@mail.startek-eng.com>
2019-08-13 10:01:02 +0800

    Getting Firebase
    sec. 14, video 142
    
    Create a new project (Expensify) on Firebase.
    Create a database with development mode.
    Add firebase to our web app.
    
    We need to create a nickname for our app first.
    After that, we can have our firebaseConfig object.
    
    Install firebase (https://www.npmjs.com/package/firebase)
    $ npm i firebase@4.2.0
    
    Create a new folder (firebase) in src folder.
    Create firebase.js in the folder. This is where we are going to connect to the database.
    So we're  going to connect to the database once in this file and then other files in our project can  use the connection by just importing what we have here.
    
    Note that we take all of the named exports from 'firebase' and dump them on a brand new variable called firebase.
    
    Paste the firebaseConfig object up on the firebase.js. And we can test our connection.
    Note that we need to import firebase.js in app.js to get the file run.
    
    Run the dev-server
    $ npm run dev-server
    
    Then we can check the database on Firebase.

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBgBO8ye2T-fn2ZeDbCrx-OSL8r6dCC1Aw",
+    authDomain: "expensify-abc3f.firebaseapp.com",
+    databaseURL: "https://expensify-abc3f.firebaseio.com",
+    projectId: "expensify-abc3f",
+    storageBucket: "",
+    messagingSenderId: "391298333468",
+    appId: "1:391298333468:web:c8db6acf5c424700"
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+// Test the connection
+firebase.database().ref().set({
+    name: 'CH Hsu'
+})
