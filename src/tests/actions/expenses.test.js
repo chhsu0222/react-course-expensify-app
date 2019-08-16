272d30419af9fba560b9a7707405d3c6f3b35b18
ch <ch@mail.startek-eng.com>
2019-08-16 14:40:57 +0800

    Testing async redux actions: part II
    sec. 14, video 154
    
    Now, we can get the actions that were dispatched to the mock store with store.getActions() and make some assertion about them.
    Then we can fetch data from Firebase with once() and see if it was actually saved over there.
    Note that we return a Promise here so we can use Promise chaining instead of nested callbacks.

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk'
 
 import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses'
 import expenses from '../fixtures/expenses'
+import database from '../../firebase/firebase'
 
 const createMockStore = configureMockStore([thunk])
 
@@ -43,13 +44,50 @@ test('should add expense to database and store', (done) => {
     }
 
     store.dispatch(startAddExpense(expenseData)).then(() => {
-        expect(1).toBe(1)
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: 'ADD_EXPENSE',
+            expense: {
+                id: expect.any(String),
+                ...expenseData
+            }
+        })
+
+        return database
+            .ref(`expenses/${actions[0].expense.id}`)
+            .once('value')
+    }).then((snapshot) => {
+        expect(snapshot.val()).toEqual(expenseData)
         done()
     })
 })
 
-test('should add expense with default to database and store', () => {
-    
+test('should add expense with default to database and store', (done) => {
+    const store = createMockStore({})
+    const defaultExpenseData = {
+        description: '',
+        amount: 0,
+        note: '',
+        createdAt: 0
+    }
+
+    store.dispatch(startAddExpense({})).then(() => {
+        const actions = store.getActions()
+        expect(actions[0]).toEqual({
+            type: 'ADD_EXPENSE',
+            expense: {
+                id: expect.any(String),
+                ...defaultExpenseData
+            }
+        })
+
+        return database
+            .ref(`expenses/${actions[0].expense.id}`)
+            .once('value')
+    }).then((snapshot) => {
+        expect(snapshot.val()).toEqual(defaultExpenseData)
+        done()
+    })
 })
 
 // test('should setup add expense action object with default values', () => {
