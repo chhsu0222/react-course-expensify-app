52b10ee404f287970c8e2248532e1e1fc358671a
ch <ch@mail.startek-eng.com>
2019-08-16 17:25:38 +0800

    Creating a separate test database
    sec. 14, video 155
    
    To setup environment variables regardless of operating system,
    lets install cross-env (https://www.npmjs.com/package/cross-env)
    $ npm i cross-env@5.0.5 --save-dev
    
    We only need to change to the script for test.
    There is no need to do it for production since it's set up for us by Heroku.
    
    Create .env.dev in the root of our project and paste the firebaseConfig into it.
    
    Create a new project (Expensify-test) on Firebase for test purpose.
    Create a database in test mode and add Firebase to our web app.
    
    Create .env.test in the root of our project and paste the firebaseConfig for test into it.
    
    Install dotenv (https://www.npmjs.com/package/dotenv) to load environment variables from a .env file into process.env.
    $ npm i dotenv@4.0.0 --save-dev
    
    https://www.npmjs.com/package/dotenv#path
    
    Setup dotenv in webpack.config.js.
    
    https://webpack.js.org/plugins/define-plugin/
    
    Note that our Node environment variables, the ones that exist in the webpack.config.js,
    they do not get passed down to the client side JavaScript.
    So we need to manually pass those through.
    
    Using dotenv in setupTest.js in tests folder.
    
    Now, when we re-run the test, it will access database for test, not the original one.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBgBO8ye2T-fn2ZeDbCrx-OSL8r6dCC1Aw",
-    authDomain: "expensify-abc3f.firebaseapp.com",
-    databaseURL: "https://expensify-abc3f.firebaseio.com",
-    projectId: "expensify-abc3f",
-    storageBucket: "",
-    messagingSenderId: "391298333468",
-    appId: "1:391298333468:web:c8db6acf5c424700"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 }
 
 // Initialize Firebase
